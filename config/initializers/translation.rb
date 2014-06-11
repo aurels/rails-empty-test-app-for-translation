b21bba019fe272efd0e46ce59e6053f19cd7c65c
Michaël Hoste <michael.hoste@gmail.com>
2014-06-11 13:36:20 +0200

    config

--- config/initializers/translation.rb
+++ config/initializers/translation.rb
@@ -1,6 +1,6 @@
 Translation.configure do |config|
   config.api_key        = "43"
-  config.endpoint       = '192.168.1.115:3001/api'
+  config.endpoint       = '192.168.1.100:3001/api'
   config.source_locale  = 'en'
-  config.target_locales = ['fr', 'de', 'nl']
+  config.target_locales = ['fr', 'cz']
 end
